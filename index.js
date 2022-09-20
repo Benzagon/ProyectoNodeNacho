e58e947af234a1a2a22d3372d3ce66c13c1147d3
Benzagon <91753390+Benzagon@users.noreply.github.com>
2022-09-20 09:06:12 -0300

    feat(Ejercicio 1-5 completados)

--- index.js
+++ index.js
@@ -2,30 +2,72 @@ const express = require("express");
 const app = express();
 const port = 9000;
 
-const users = [
-    {id: 1, name: "John Doe"},
-    {id: 2, name: "Jane Smith"},
-    {id: 3, name: "Marco Polo"},
-];
+app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.send("API running OK ...")
+const menu = require("./menu.json")
+
+// const users = [
+//     {id: 1, name: "John Doe"},
+//     {id: 2, name: "Jane Smith"},
+//     {id: 3, name: "Marco Polo"},
+// ];
+
+ app.get("/", (req, res) => {
+     res.send("API running OK ...")
+ });
+
+//Ejercicio 1
+app.get("/menu", (req, res) => {
+     res.send(menu);
 });
 
-app.get("/users", (req, res) => {
-    res.send(users);
+//Ejercicio 2
+app.get("/menu/:id", (req, res) =>{
+   const id = parseInt(req.params.id);
+   const plate = menu.find((plate) => plate.id === id);
+    
+   if(!plate)
+       res.status(404).send("Dish not found...");  
+   else
+       res.send(plate);
 });
 
-app.get("/users/:id", (req, res) =>{
-    const id = parseInt(req.params.id);
-    const user = users.find((user) => user.id === id);
-
-    if(!user)
-        res.status(404).send("User not found")
-    else
-        res.status(200).json(user)
+//Ejercicio 3
+app.get("/principales", (req, res) =>{
+    const principales = menu.filter((dish) => dish.tipo === "principal");
+    res.send(principales);
 });
 
-app.listen(port, () =>{
+//Ejercicio 4
+app.get("/postres", (req, res) =>{
+    const postres = menu.filter((dish) => dish.tipo === "postre");
+    res.send(postres);
+});
+
+//Ejercicio 5
+app.get("/bebidas", (req, res) =>{
+    const bebidas = menu.filter((dish) => dish.tipo === "bebida");
+    res.send(bebidas);
+});
+
+//Ejercicio 6
+app.post("/pedido", (req, res) =>{
+    const pedido = req.body;
+    const dishesOrdered = menu.filter((dish) => dish.id === pedido.id)
+    
+    console.log(dishesOrdered)
+    res.json(pedido)
+});
+// app.get("/users/:id", (req, res) =>{
+//     const id = parseInt(req.params.id);
+//     const user = users.find((user) => user.id === id);
+
+//     if(!user)
+//         res.status(404).send("User not found")
+//     else
+//         res.status(200).json(user)
+// });
+
+ app.listen(port, () =>{
     console.log(`> Server running on port ${port}`)
-});
+ });
